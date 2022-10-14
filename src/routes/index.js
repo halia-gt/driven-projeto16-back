f84a49fe3d3dea6c9a787ccf8500519d0ae0f305
Nathalia <nathalia.gramst@gmail.com>
2022-10-14 15:41:16 -0300

    Router for urls

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import usersRouters from "./users.routers.js";
+import urlsRouters from "./urls.routers.js";
 
 const router = express.Router();
 router.use(usersRouters);
+router.use(urlsRouters);
 
 export default router;
