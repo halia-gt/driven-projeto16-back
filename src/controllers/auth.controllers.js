f8355a834b991bc3f54b1b9dcacec039bdd1be00
Nathalia <nathalia.gramst@gmail.com>
2022-10-15 22:04:33 -0300

    Adding correctly the Id on sessions

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -37,7 +37,7 @@ async function singIn(req, res) {
             userId: user.id
         }, process.env.TOKEN_SECRET);
 
-        await authRepository.signIn(user.userId, token);
+        await authRepository.signIn(user.id, token);
 
         res.status(200).send({ token });
     } catch (error) {
